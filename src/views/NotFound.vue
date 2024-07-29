24997eec62ecc47bd17395fb3ddd90cce7544223
yuto0226 <wayne71112@gmail.com>
2024-07-30 00:52:07 +0800

    Update 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,7 @@
 <script></script>
 <template>
-    <div>
-        <h1>404 Not Found</h1>
-        <p>找不到這個網址的網頁餒</p>
-    </div>
+  <div>
+    <h1>404 Not Found</h1>
+    <p>找不到這個網址的網頁餒</p>
+  </div>
 </template>
