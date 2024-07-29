43a6968be5ffec2f4e04a66d8e6a4815f972be48
yuto0226 <yuto0226@users.noreply.github.com>
2024-07-29 16:27:03 +0000

    deploy: 413357ffa2a0537480735bd57c4a45f455108938

---
+++ assets/RankView-BLDMA9rB.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o,a as t}from"./index-CWx4Fewl.js";const n={},s=t("h1",null,"排行榜",-1),a=[s];function _(r,l){return o(),c("div",null,a)}const i=e(n,[["render",_]]);export{i as default};
--- assets/RankView-VU60L1uH.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as n}from"./index-CFZPkxh1.js";const a={};function o(r,t){return n(),c("h1",null,"This Rank Page")}const _=e(a,[["render",o]]);export{_ as default};
