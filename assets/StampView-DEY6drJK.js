0e7fdf2b60588e7b6058de4d0f79a1551a5f1ca3
yuto0226 <yuto0226@users.noreply.github.com>
2024-07-29 16:44:56 +0000

    deploy: 618b2a64aa568a54f898a8d97591d0d8072972d5

--- assets/StampView-DEY6drJK.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as c,a as o}from"./index-DYErRIfK.js";const s={},n=o("h1",null,"蓋章！",-1),a=[n];function _(r,l){return c(),t("div",null,a)}const i=e(s,[["render",_]]);export{i as default};
