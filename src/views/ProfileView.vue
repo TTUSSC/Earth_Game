4a004789c24952d2329f32cf716cd5c29415ef6a
yuto0226 <wayne71112@gmail.com>
2024-07-30 12:48:01 +0800

    Update user page and title style
    font family set to GenWanMin
    and fetch url id

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,8 +1,12 @@
-<script>
+<script setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
 </script>
 <template>
     <div>
         <h1>Profile</h1>
-        <span>user</span>
+        <span>{{ route.params.id }}</span>
     </div>
 </template>
