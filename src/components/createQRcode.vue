9bf2fbf48a751e1b41489dfc387a5f2874099191
yuto0226 <wayne71112@gmail.com>
2024-07-31 13:12:16 +0800

    Format and Push

--- src/components/createQRcode.vue
+++ src/components/createQRcode.vue
@@ -1,6 +1,9 @@
 <script></script>
 <template>
-    <img class="qrcode img-fluid rounded"
-        src="https://quickchart.io/qr?size=1080&margin=2&text=https://ttussc.github.io/Earth_Game" alt="" />
+  <img
+    class="qrcode img-fluid rounded"
+    src="https://quickchart.io/qr?size=1080&margin=2&text=https://ttussc.github.io/Earth_Game"
+    alt=""
+  />
 </template>
 <style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,22 @@
 <script setup>
-import createQRcode from '../components/createQRcode.vue';
+import createQRcode from '../components/createQRcode.vue'
 </script>
 <template>
   <div class="container w-75">
     <h1>社博大地遊戲</h1>
     <p class="d-inline-flex gap-1">
-      <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample"
-        aria-expanded="false" aria-controls="collapseExample">
+      <button
+        class="btn btn-primary"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#collapseExample"
+        aria-expanded="false"
+        aria-controls="collapseExample"
+      >
         掃描 QRcode
       </button>
     </p>
-    <div class="collapse mb-3 w-50 mx-auto" id="collapseExample">
+    <div class="collapse mb-3 mx-auto" id="collapseExample">
       <div class="card card-body">
         <createQRcode />
       </div>
