21af9a82117592f00cfbf58eab0f47425053e6da
yuto0226 <wayne71112@gmail.com>
2024-07-31 12:52:30 +0800

    Adjusting QRcode size

---
+++ src/components/createQRcode.vue
@@ -0,0 +1,6 @@
+<script></script>
+<template>
+    <img class="qrcode img-fluid rounded"
+        src="https://quickchart.io/qr?size=1080&margin=2&text=https://ttussc.github.io/Earth_Game" alt="" />
+</template>
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,18 @@
-<script></script>
+<script setup>
+import createQRcode from '../components/createQRcode.vue';
+</script>
 <template>
   <div class="container w-75">
     <h1>社博大地遊戲</h1>
     <p class="d-inline-flex gap-1">
-      <button
-        class="btn btn-primary"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#collapseExample"
-        aria-expanded="false"
-        aria-controls="collapseExample"
-      >
+      <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample"
+        aria-expanded="false" aria-controls="collapseExample">
         掃描 QRcode
       </button>
     </p>
-    <div class="collapse" id="collapseExample">
-      <div class="card card-body text-center">
-        <img
-          class="img-fluid mt-2 mb-4 rounded"
-          src="https://quickchart.io/qr?size=500&margin=2&text=https://ttussc.github.io/Earth_Game"
-          alt=""
-        />
+    <div class="collapse mb-3 w-50 mx-auto" id="collapseExample">
+      <div class="card card-body">
+        <createQRcode />
       </div>
     </div>
   </div>
