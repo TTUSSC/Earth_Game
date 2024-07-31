b81c7650a709b4abf5e6fdf598f8cff08bc93c43
yuto0226 <wayne71112@gmail.com>
2024-07-31 16:57:57 +0800

    Adjust home page button margin

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,18 +4,10 @@ import createQRcode from '../components/createQRcode.vue'
 <template>
   <div>
     <h1>社博大地遊戲</h1>
-    <p class="d-inline-flex gap-1">
-      <button
-        class="btn btn-primary"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#collapseExample"
-        aria-expanded="false"
-        aria-controls="collapseExample"
-      >
-        掃描 QRcode
-      </button>
-    </p>
+    <button class="btn btn-primary my-2" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample"
+      aria-expanded="false" aria-controls="collapseExample">
+      掃描 QRcode
+    </button>
     <div class="collapse mb-3 mx-auto" id="collapseExample">
       <div class="card card-body">
         <createQRcode />
