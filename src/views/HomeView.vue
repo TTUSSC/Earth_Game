c597a3878f6fcb6a0d141c954f6d98b32a66b1d1
yuto0226 <wayne71112@gmail.com>
2024-07-31 12:14:38 +0800

    Add home page qrcode

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,5 +2,17 @@
 <template>
   <div class="container w-75">
     <h1>社博大地遊戲</h1>
+    <p class="d-inline-flex gap-1">
+      <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample"
+        aria-expanded="false" aria-controls="collapseExample">
+        掃描 QRcode
+      </button>
+    </p>
+    <div class="collapse" id="collapseExample">
+      <div class="card card-body text-center">
+        <img class="img-fluid mt-2 mb-4 rounded"
+          src="https://quickchart.io/qr?size=500&margin=2&text=https://ttussc.github.io/Earth_Game" alt="">
+      </div>
+    </div>
   </div>
 </template>
