ad9985c5c3ba86b6882e7c51a910a76c62e0da08
yuto0226 <wayne71112@gmail.com>
2024-08-06 18:08:36 +0800

    Update methods

--- src/stores/useRecordsStore.js
+++ src/stores/useRecordsStore.js
@@ -17,6 +17,42 @@ export const useRecordsStore = defineStore('records', {
             this.data = response.data;
             console.log('useRecordsStore:');
             console.log(this.data);
+        },
+        async query_record(club_email, user_email) {
+            await this.callAPI();
+            for (let i = 0; i < this.data.length; i++) {
+                if (this.data['club_email'] === club_email && this.data['user_email'] === user_email) {
+                    console.log("record found:");
+                    console.log(this.data[i]);
+                    return this.data[i];
+                }
+            }
+            console.log("record not found.");
+            return false;
+        },
+        async query_by_user(user_email) {
+            await this.callAPI();
+            for (let i = 0; i < this.data.length; i++) {
+                if (this.data['user_email'] === user_email) {
+                    console.log("record found:");
+                    console.log(this.data[i]);
+                    return this.data[i];
+                }
+            }
+            console.log("record not found.");
+            return false;
+        },
+        async query_by_club(club_email) {
+            await this.callAPI();
+            for (let i = 0; i < this.data.length; i++) {
+                if (this.data['club_email'] === club_email) {
+                    console.log("record found:");
+                    console.log(this.data[i]);
+                    return this.data[i];
+                }
+            }
+            console.log("record not found.");
+            return false;
         }
     },
     getters: {}
