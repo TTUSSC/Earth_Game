bd6e77fa61150f50ab0e9b8e91804f76fe02b279
yuto0226 <wayne71112@gmail.com>
2024-08-08 18:16:08 +0800

    Update NavBar
    Make navbar unshow when nav to other page

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,25 @@
 <script setup>
+import { ref } from 'vue';
 import { useAuthStore } from '@/stores/useAuthStore';
+import { useRouter } from 'vue-router';
 const authStore = useAuthStore();
+const router = useRouter();
+const isNavExpanded = ref(false);
+
+function toggleNav() {
+  isNavExpanded.value = !isNavExpanded.value;
+  if (!isNavExpanded.value) {
+    document.getElementById('navbarCollapse').classList.add('show');
+    console.log("show");
+  } else {
+    document.getElementById('navbarCollapse').classList.remove('show');
+  }
+}
+
+function navigateTo(path) {
+  router.push(path);
+  toggleNav(); // 縮回 navbar
+}
 </script>
 
 <template>
@@ -8,30 +27,31 @@ const authStore = useAuthStore();
     <div class="container-fluid">
       <RouterLink to="/" class="navbar-brand">
         <img src="/SSC_logo.png" height="32px" />
-        <span class="title">社團博覽會大地遊戲</span>
+        <span>社團博覽會大地遊戲</span>
       </RouterLink>
 
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-        aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" @click="toggleNav()" type="button" data-bs-toggle="collapse"
+        data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false"
+        aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item">
-            <RouterLink to="/" class="nav-link">首頁</RouterLink>
+            <button @click="navigateTo('/')" class="nav-link">首頁</button>
           </li>
           <li class="nav-item" v-if="!authStore.is_club">
-            <RouterLink to="/stamp" class="nav-link">蓋章</RouterLink>
+            <button @click="navigateTo('/stamp')" class="nav-link">蓋章</button>
           </li>
           <li class="nav-item">
-            <RouterLink to="/partner" class="nav-link">合作夥伴</RouterLink>
+            <button @click="navigateTo('/partner')" class="nav-link">合作夥伴</button>
           </li>
           <li class="nav-item" v-if="!authStore.isLoggedIn">
-            <RouterLink to="/register" class="nav-link">註冊</RouterLink>
+            <button @click="navigateTo('/register')" class="nav-link">註冊</button>
           </li>
           <li class="nav-item" v-if="!authStore.isLoggedIn || authStore.is_club">
-            <RouterLink to="/scan" class="nav-link">我是社團</RouterLink>
+            <button @click="navigateTo('/scan')" class="nav-link">我是社團</button>
           </li>
         </ul>
       </div>
