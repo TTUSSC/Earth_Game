dcab1904e137874f59e6cbf3110196d6e6d6c3b9
yuto0226 <wayne71112@gmail.com>
2024-08-22 18:22:54 +0800

    Update ScanView
    新增兌換按鈕，可以在兌換抽獎券時使用，使用後會鎖定帳號

--- src/views/ScanView.vue
+++ src/views/ScanView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import QRcodeReader from '@/components/QRcodeReader.vue';
 import { useAuthStore } from '@/stores/useAuthStore';
 import { useClubsStore } from '@/stores/useClubsStore';
@@ -37,7 +37,7 @@ const sendStamp = async () => {
         isLoading.value = false;
         return;
     }
-    if (await recordExist(authStore.email, scanEmail.value, is_IG.value)) {
+    if (await recordExist(authStore.email, scanEmail.value, scan_type.value == SCAN_IG)) {
         isLoading.value = false;
         return;
     }
@@ -45,7 +45,7 @@ const sendStamp = async () => {
         const formData = new FormData();
         formData.append("entry.1180634340", authStore.email);
         formData.append("entry.1788223982", scanEmail.value);
-        formData.append("entry.914325063", is_IG.value);
+        formData.append("entry.914325063", scan_type.value == SCAN_IG);
 
         await
             fetch('https://docs.google.com/forms/u/2/d/e/1FAIpQLScMibiJl3amUlq6x91EwApaW7XqnRTl51ZQagvWz3vNftR3tw/formResponse', {
@@ -53,7 +53,7 @@ const sendStamp = async () => {
                 mode: 'no-cors',
                 body: formData
             });
-        console.log('Form submitted successfully')
+        console.log('Form submitted successfully');
         isError.value = false;
         pageMsg.value = "提交 " + scanName.value + " 蓋章紀錄成功！";
     } catch (error) {
@@ -71,6 +71,45 @@ const sendStamp = async () => {
     // } else {
     // console.log("submit failed!");
     // }
+}
+
+const exangeStamp = async () => {
+    isLoading.value = true;
+    console.log("exangeStamp() update user data.");
+    await usersStore.callAPI();
+    const user = await usersStore.get_user_by_email(scanEmail.value);
+
+    if (user.access_priv == false) {
+        isError.value = true;
+        pageMsg.value = "帳號已經兌換過抽獎券了";
+        isLoading.value = false;
+        return;
+    }
+
+    try {
+        //
+        const formData = new FormData();
+        formData.append("entry.1180634340", authStore.email);   // 負責人
+        formData.append("entry.1788223982", scanEmail.value);   // 使用者
+
+        await
+            fetch('https://docs.google.com/forms/u/2/d/e/1FAIpQLSfyjgMo-YodfYApI5mg4kTPhLOQR8oeD_Ey5LXhHqWrhXtSHA/formResponse', {
+                method: 'POST',
+                mode: 'no-cors',
+                body: formData
+            });
+        console.log('Form submitted successfully');
+        isError.value = false;
+        pageMsg.value = "提交 " + scanName.value + " 兌換紀錄成功！";
+    } catch (error) {
+        //
+        isError.value = true;
+        pageMsg.value = "紀錄提交錯誤：" + error;
+        console.error("Error submitting form:", error);
+    } finally {
+        //
+        isLoading.value = false;
+    }
 }
 
 const recordExist = async (club_email, user_email, is_IG) => {
@@ -114,32 +153,52 @@ const handleScanSuccess = async (data) => {
     // check stamp
 
     // update 資料庫
-    await sendStamp();
+    if (scan_type.value == SCAN_IG || scan_type.value == SCAN_STAMP)
+        await sendStamp();
+    else {
+        await exangeStamp();
+    }
     waiting.value = false;
 }
 
 const pageMsg = ref('');
 const isError = ref(false);
 
-const is_IG = ref(false);
-let toggle_type_btn = () => {
-    is_IG.value = !is_IG.value;
-}
+const SCAN_STAMP = 0;
+const SCAN_IG = 1;
+const SCAN_LOCK = 2;
+
+const scan_type = ref(SCAN_STAMP);
+
+const type_name = computed(() => {
+    if (scan_type.value == SCAN_STAMP) return "掃描";
+    else if (scan_type.value == SCAN_IG) return "限動";
+    else if (scan_type.value == SCAN_LOCK) return "兌換";
+    return null;
+})
 </script>
 
 <template>
     <div>
         <div v-if="authStore.isLoggedIn && authStore.is_club">
-            <h1 class="mb-4">{{ authStore.name }}：{{ is_IG ? '限動' : '掃描' }}</h1>
+            <h1 class="mb-4">{{ authStore.name }}：{{ type_name ? type_name : "錯誤" }}</h1>
             <div v-if="pageMsg" class="mt-3 text-break " :class="['alert', isError ? 'alert-danger' : 'alert-success']"
                 role="alert">
                 {{ pageMsg }}
             </div>
             <QRcodeReader @scan-success="handleScanSuccess" />
-            <div class="row mb-4">
-                <button class="d-grid col-6 mx-auto btn" @click="toggle_type_btn"
-                    :class="{ 'btn-warning': is_IG, 'btn-success': !is_IG }">
-                    {{ is_IG ? 'IG 打卡掃描' : '活動蓋章掃描' }}
+            <div class="row mb-2 mx-5">
+                <button class="d-grid col mx-2 btn btn-outline-success" @click="scan_type = SCAN_STAMP"
+                    :class="{ active: scan_type == SCAN_STAMP }">
+                    蓋章
+                </button>
+                <button class="d-grid col mx-2 btn btn-outline-success" @click="scan_type = SCAN_IG"
+                    :class="{ active: scan_type == SCAN_IG }">
+                    IG
+                </button>
+                <button class="d-grid col mx-2 btn btn-outline-success" @click="scan_type = SCAN_LOCK"
+                    :class="{ active: scan_type == SCAN_LOCK }">
+                    鎖定
                 </button>
             </div>
         </div>
