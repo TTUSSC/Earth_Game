979251896184530389ad9fc020c7edc425dad970
yuto0226 <wayne71112@gmail.com>
2024-08-29 13:17:02 +0800

    Update deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,10 @@
 npm run build
 copy .\dist\index.html .\public\404.html
+copy .\dist\index.html .\public\rules.html
+copy .\dist\index.html .\public\stamp.html
+copy .\dist\index.html .\public\register.html
+copy .\dist\index.html .\public\rules.html
+copy .\dist\index.html .\public\club_login.html
 git add .\public\404.html
 git commit -m "Update 404.html"
 git push
