682f7c466c472ab5e8575d53de22538ce9bae580
yuto0226 <wayne71112@gmail.com>
2024-08-29 13:19:00 +0800

    Update deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -6,5 +6,10 @@ copy .\dist\index.html .\public\register.html
 copy .\dist\index.html .\public\rules.html
 copy .\dist\index.html .\public\club_login.html
 git add .\public\404.html
+git add .\public\rules.html
+git add .\public\stamp.html
+git add .\public\register.html
+git add .\public\rules.html
+git add .\public\club_login.html
 git commit -m "Update 404.html"
 git push
